89e96685ef1c5b76bc4ab71acbe9f0d3fc375fcc
Anna <a.falkonia@gmail.com>
2022-08-19 15:58:44 +0300

    Fix store import

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
-import store from './store'
+import store from './redux'
 import App from './App';
 
 const root = ReactDOM.createRoot(
