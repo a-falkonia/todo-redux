7c0d9571279c4f3dd26dc49a0b698aeb1bf58178
Anna <a.falkonia@gmail.com>
2022-09-07 21:54:05 +0300

    Fix nested styled-components in FilterPanel

--- src/components/FilterPanel.tsx
+++ src/components/FilterPanel.tsx
@@ -1,34 +1,35 @@
 import styled from 'styled-components';
 
-const FilterPanel = () => {
-    const StyledPanel = styled.div`
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    padding: 16px 0px;
-  `;
-  const StyledButton = styled.button`
-    display: inline-flex;
-    flex: 1;
-    position: relative;
-    align-items: center;
-    justify-content: center;
-    overflow: hidden;
-    height: 40px;
-    padding: 0px 16px;
-    font-size: 18px;
-    font-family: monospace;
-    color: #36395a;
-    background-color: white;
-    border-color: #4c8bf5;
-    border-radius: 4px;
-    user-select: none;
-    cursor: pointer;
+const StyledPanel = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 16px 0px;
+`;
+const StyledButton = styled.button`
+  display: inline-flex;
+  flex: 1;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  height: 40px;
+  padding: 0px 16px;
+  font-size: 18px;
+  font-family: monospace;
+  color: #36395a;
+  background-color: white;
+  border-color: #4c8bf5;
+  border-radius: 4px;
+  user-select: none;
+  cursor: pointer;
 
-    :not(:first-child){
-        margin-left: 8px;
-    }
-  `;
+  :not(:first-child) {
+    margin-left: 8px;
+  }
+`;
+
+const FilterPanel = () => {
   return (
     <StyledPanel>
       <StyledButton>Show All</StyledButton>
